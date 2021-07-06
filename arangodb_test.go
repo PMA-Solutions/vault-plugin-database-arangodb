9e4ca4298cf97b3939146d8059122dd37ba68100
Luis Miguel Silva Furnas <luis@pmasolutions.com>
2021-07-06 10:51:34 +0000

    Reindent files with 4 spaces

--- arangodb_test.go
+++ arangodb_test.go
@@ -1,164 +1,164 @@
 package arangodb
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/url"
-	"os"
-	"reflect"
-	"testing"
-	"time"
+    "context"
+    "fmt"
+    "log"
+    "net/url"
+    "os"
+    "reflect"
+    "testing"
+    "time"
 
-	"github.com/arangodb/go-driver"
-	"github.com/arangodb/go-driver/http"
-	"github.com/hashicorp/errwrap"
-	"github.com/hashicorp/vault/helper/testhelpers/docker"
-	dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
-	dbtesting "github.com/hashicorp/vault/sdk/database/dbplugin/v5/testing"
+    "github.com/arangodb/go-driver"
+    "github.com/arangodb/go-driver/http"
+    "github.com/hashicorp/errwrap"
+    "github.com/hashicorp/vault/helper/testhelpers/docker"
+    dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
+    dbtesting "github.com/hashicorp/vault/sdk/database/dbplugin/v5/testing"
 )
 
 const arangoEmptyGrants = `{}`
 
 type Config struct {
-	docker.ServiceURL
-	Username string
-	Password string
+    docker.ServiceURL
+    Username string
+    Password string
 }
 
 var _ docker.ServiceConfig = &Config{}
 
 func (c *Config) clientConfig() driver.ClientConfig {
-	conn, err := http.NewConnection(http.ConnectionConfig{
-		Endpoints: []string{c.URL().String()},
-	})
+    conn, err := http.NewConnection(http.ConnectionConfig{
+        Endpoints: []string{c.URL().String()},
+    })
 
-	if err != nil {
-		log.Fatalf("Unable to create connection: %s", err)
-	}
+    if err != nil {
+        log.Fatalf("Unable to create connection: %s", err)
+    }
 
-	return driver.ClientConfig{
-		Connection:     conn,
-		Authentication: driver.BasicAuthentication(c.Username, c.Password),
-	}
+    return driver.ClientConfig{
+        Connection:     conn,
+        Authentication: driver.BasicAuthentication(c.Username, c.Password),
+    }
 }
 
 func copyConfig(config map[string]interface{}) map[string]interface{} {
-	newConfig := map[string]interface{}{}
-	for k, v := range config {
-		newConfig[k] = v
-	}
-	return newConfig
+    newConfig := map[string]interface{}{}
+    for k, v := range config {
+        newConfig[k] = v
+    }
+    return newConfig
 }
 
 func prepareArangoDBTestContainer(t *testing.T) (func(), *Config) {
-	c := &Config{
-		Username: "root",
-		Password: "root",
-	}
-	if host := os.Getenv("ARANGODB_HOST"); host != "" {
-		c.ServiceURL = *docker.NewServiceURL(url.URL{Scheme: "http", Host: host})
-		return func() {}, c
-	}
+    c := &Config{
+        Username: "root",
+        Password: "root",
+    }
+    if host := os.Getenv("ARANGODB_HOST"); host != "" {
+        c.ServiceURL = *docker.NewServiceURL(url.URL{Scheme: "http", Host: host})
+        return func() {}, c
+    }
 
-	runner, err := docker.NewServiceRunner(docker.RunOptions{
-		ImageRepo: "arangodb",
-		ImageTag:  "3.7.11",
-		Env: []string{
-			"ARANGO_ROOT_PASSWORD=" + c.Password,
-		},
-		Ports: []string{"8529/tcp"},
-	})
-	if err != nil {
-		t.Fatalf("Could not start docker ArangoDB: %s", err)
-	}
-	svc, err := runner.StartService(context.Background(), func(ctx context.Context, host string, port int) (docker.ServiceConfig, error) {
-		c.ServiceURL = *docker.NewServiceURL(url.URL{
-			Scheme: "http",
-			Host:   fmt.Sprintf("%s:%d", host, port),
-		})
+    runner, err := docker.NewServiceRunner(docker.RunOptions{
+        ImageRepo: "arangodb",
+        ImageTag:  "3.7.11",
+        Env: []string{
+            "ARANGO_ROOT_PASSWORD=" + c.Password,
+        },
+        Ports: []string{"8529/tcp"},
+    })
+    if err != nil {
+        t.Fatalf("Could not start docker ArangoDB: %s", err)
+    }
+    svc, err := runner.StartService(context.Background(), func(ctx context.Context, host string, port int) (docker.ServiceConfig, error) {
+        c.ServiceURL = *docker.NewServiceURL(url.URL{
+            Scheme: "http",
+            Host:   fmt.Sprintf("%s:%d", host, port),
+        })
 
-		client, err := driver.NewClient(c.clientConfig())
-		if err != nil {
-			return nil, errwrap.Wrapf("error creating ArangoDB client: {{err}}", err)
-		}
+        client, err := driver.NewClient(c.clientConfig())
+        if err != nil {
+            return nil, errwrap.Wrapf("error creating ArangoDB client: {{err}}", err)
+        }
 
-		_, err = client.Version(ctx)
-		if err != nil {
-			return nil, errwrap.Wrapf("error checking cluster status: {{err}}", err)
-		}
+        _, err = client.Version(ctx)
+        if err != nil {
+            return nil, errwrap.Wrapf("error checking cluster status: {{err}}", err)
+        }
 
-		return c, nil
-	})
-	if err != nil {
-		t.Fatalf("Could not start docker ArangoDB: %s", err)
-	}
+        return c, nil
+    })
+    if err != nil {
+        t.Fatalf("Could not start docker ArangoDB: %s", err)
+    }
 
-	return svc.Cleanup, svc.Config.(*Config)
+    return svc.Cleanup, svc.Config.(*Config)
 }
 
 func TestArangoDB_Initialize(t *testing.T) {
-	cleanup, config := prepareArangoDBTestContainer(t)
-	defer cleanup()
+    cleanup, config := prepareArangoDBTestContainer(t)
+    defer cleanup()
 
-	db := Connect()
-	defer dbtesting.AssertClose(t, db)
+    db := Connect()
+    defer dbtesting.AssertClose(t, db)
 
-	pluginConfig := map[string]interface{}{
-		"connection_url": config.URL().String(),
-		"username":       "root",
-		"password":       "root",
-	}
+    pluginConfig := map[string]interface{}{
+        "connection_url": config.URL().String(),
+        "username":       "root",
+        "password":       "root",
+    }
 
-	// Make a copy since the original map could be modified by the Initialize call
-	expectedConfig := copyConfig(pluginConfig)
+    // Make a copy since the original map could be modified by the Initialize call
+    expectedConfig := copyConfig(pluginConfig)
 
-	req := dbplugin.InitializeRequest{
-		Config:           pluginConfig,
-		VerifyConnection: true,
-	}
+    req := dbplugin.InitializeRequest{
+        Config:           pluginConfig,
+        VerifyConnection: true,
+    }
 
-	resp := dbtesting.AssertInitialize(t, db, req)
+    resp := dbtesting.AssertInitialize(t, db, req)
 
-	if !reflect.DeepEqual(resp.Config, expectedConfig) {
-		t.Fatalf("Actual config: %#v\nExpected config: %#v", resp.Config, expectedConfig)
-	}
+    if !reflect.DeepEqual(resp.Config, expectedConfig) {
+        t.Fatalf("Actual config: %#v\nExpected config: %#v", resp.Config, expectedConfig)
+    }
 
-	if !db.Initialized {
-		t.Fatal("Database should be initialized")
-	}
+    if !db.Initialized {
+        t.Fatal("Database should be initialized")
+    }
 }
 
 func TestMongoDB_CreateUser(t *testing.T) {
-	cleanup, config := prepareArangoDBTestContainer(t)
-	defer cleanup()
+    cleanup, config := prepareArangoDBTestContainer(t)
+    defer cleanup()
 
-	db := Connect()
-	defer dbtesting.AssertClose(t, db)
+    db := Connect()
+    defer dbtesting.AssertClose(t, db)
 
-	initReq := dbplugin.InitializeRequest{
-		Config: map[string]interface{}{
-			"connection_url": config.URL().String(),
-			"username":       "root",
-			"password":       "root",
-		},
-		VerifyConnection: true,
-	}
-	dbtesting.AssertInitialize(t, db, initReq)
+    initReq := dbplugin.InitializeRequest{
+        Config: map[string]interface{}{
+            "connection_url": config.URL().String(),
+            "username":       "root",
+            "password":       "root",
+        },
+        VerifyConnection: true,
+    }
+    dbtesting.AssertInitialize(t, db, initReq)
 
-	password := "myreallysecurepassword"
-	createReq := dbplugin.NewUserRequest{
-		UsernameConfig: dbplugin.UsernameMetadata{
-			DisplayName: "test",
-			RoleName:    "test",
-		},
-		Statements: dbplugin.Statements{
-			Commands: []string{arangoEmptyGrants},
-		},
-		Password:   password,
-		Expiration: time.Now().Add(time.Minute),
-	}
-	_ = dbtesting.AssertNewUser(t, db, createReq)
+    password := "myreallysecurepassword"
+    createReq := dbplugin.NewUserRequest{
+        UsernameConfig: dbplugin.UsernameMetadata{
+            DisplayName: "test",
+            RoleName:    "test",
+        },
+        Statements: dbplugin.Statements{
+            Commands: []string{arangoEmptyGrants},
+        },
+        Password:   password,
+        Expiration: time.Now().Add(time.Minute),
+    }
+    _ = dbtesting.AssertNewUser(t, db, createReq)
 
-	// todo: assertCredsExist(t, createResp.Username, password, connURL)
+    // todo: assertCredsExist(t, createResp.Username, password, connURL)
 }
--- connection_producer.go
+++ connection_producer.go
@@ -1,123 +1,123 @@
 package arangodb
 
 import (
-	"context"
-	"fmt"
-	"sync"
+    "context"
+    "fmt"
+    "sync"
 
-	driver "github.com/arangodb/go-driver"
-	"github.com/arangodb/go-driver/http"
-	dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
-	"github.com/hashicorp/vault/sdk/database/helper/connutil"
-	"github.com/mitchellh/mapstructure"
+    driver "github.com/arangodb/go-driver"
+    "github.com/arangodb/go-driver/http"
+    dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
+    "github.com/hashicorp/vault/sdk/database/helper/connutil"
+    "github.com/mitchellh/mapstructure"
 )
 
 // arangoDBConnectionProducer implements ConnectionProducer and provides an
 // interface for databases to make connections.
 type arangoDBConnectionProducer struct {
-	Username      string `json:"username" structs:"username" mapstructure:"username"`
-	Password      string `json:"password" structs:"password" mapstructure:"password"`
-	ConnectionURL string `json:"connection_url" structs:"connection_url" mapstructure:"connection_url"`
+    Username      string `json:"username" structs:"username" mapstructure:"username"`
+    Password      string `json:"password" structs:"password" mapstructure:"password"`
+    ConnectionURL string `json:"connection_url" structs:"connection_url" mapstructure:"connection_url"`
 
-	rawConfig map[string]interface{}
+    rawConfig map[string]interface{}
 
-	Initialized bool
-	Type        string
-	client      driver.Client
-	sync.Mutex
+    Initialized bool
+    Type        string
+    client      driver.Client
+    sync.Mutex
 }
 
 func (a *arangoDBConnectionProducer) secretValues() map[string]string {
-	return map[string]string{
-		a.Password: "[password]",
-		a.Password: "[password]",
-	}
+    return map[string]string{
+        a.Password: "[password]",
+        a.Password: "[password]",
+    }
 }
 
 func (a *arangoDBConnectionProducer) Initialize(ctx context.Context, req dbplugin.InitializeRequest) (dbplugin.InitializeResponse, error) {
-	a.Lock()
-	defer a.Unlock()
+    a.Lock()
+    defer a.Unlock()
 
-	a.rawConfig = req.Config
+    a.rawConfig = req.Config
 
-	err := mapstructure.WeakDecode(req.Config, a)
-	if err != nil {
-		return dbplugin.InitializeResponse{}, err
-	}
+    err := mapstructure.WeakDecode(req.Config, a)
+    if err != nil {
+        return dbplugin.InitializeResponse{}, err
+    }
 
-	// Set initialized to true at this point since all fields are set,
-	// and the connection can be established at a later time.
-	a.Initialized = true
+    // Set initialized to true at this point since all fields are set,
+    // and the connection can be established at a later time.
+    a.Initialized = true
 
-	if req.VerifyConnection {
-		_, err := a.Connection(ctx)
-		if err != nil {
-			return dbplugin.InitializeResponse{}, fmt.Errorf("failed to verify connection: %w", err)
-		}
+    if req.VerifyConnection {
+        _, err := a.Connection(ctx)
+        if err != nil {
+            return dbplugin.InitializeResponse{}, fmt.Errorf("failed to verify connection: %w", err)
+        }
 
-		_, err = a.client.Version(ctx)
-		if err != nil {
-			return dbplugin.InitializeResponse{}, fmt.Errorf("failed to verify connection: %w", err)
-		}
-	}
+        _, err = a.client.Version(ctx)
+        if err != nil {
+            return dbplugin.InitializeResponse{}, fmt.Errorf("failed to verify connection: %w", err)
+        }
+    }
 
-	resp := dbplugin.InitializeResponse{
-		Config: req.Config,
-	}
+    resp := dbplugin.InitializeResponse{
+        Config: req.Config,
+    }
 
-	return resp, nil
+    return resp, nil
 }
 
 // Connection creates a database connection
 func (a *arangoDBConnectionProducer) Connection(ctx context.Context) (interface{}, error) {
-	if !a.Initialized {
-		return nil, connutil.ErrNotInitialized
-	}
+    if !a.Initialized {
+        return nil, connutil.ErrNotInitialized
+    }
 
-	// If we already have a DB, return it
-	if a.client != nil {
-		return a.client, nil
-	}
+    // If we already have a DB, return it
+    if a.client != nil {
+        return a.client, nil
+    }
 
-	client, err := a.createClient()
-	if err != nil {
-		return nil, err
-	}
+    client, err := a.createClient()
+    if err != nil {
+        return nil, err
+    }
 
-	//  Store the session in backend for reuse
-	a.client = client
+    //  Store the session in backend for reuse
+    a.client = client
 
-	return a.client, nil
+    return a.client, nil
 }
 
 func (a *arangoDBConnectionProducer) createClient() (driver.Client, error) {
-	conn, err := http.NewConnection(http.ConnectionConfig{
-		Endpoints: []string{a.ConnectionURL},
-	})
-	if err != nil {
-		return nil, err
-	}
+    conn, err := http.NewConnection(http.ConnectionConfig{
+        Endpoints: []string{a.ConnectionURL},
+    })
+    if err != nil {
+        return nil, err
+    }
 
-	auth := driver.BasicAuthentication(a.Username, a.Password)
+    auth := driver.BasicAuthentication(a.Username, a.Password)
 
-	c, err := driver.NewClient(driver.ClientConfig{
-		Connection:     conn,
-		Authentication: auth,
-	})
+    c, err := driver.NewClient(driver.ClientConfig{
+        Connection:     conn,
+        Authentication: auth,
+    })
 
-	if err != nil {
-		return nil, err
-	}
+    if err != nil {
+        return nil, err
+    }
 
-	return c, nil
+    return c, nil
 }
 
 // Close terminates the database connection.
 func (a *arangoDBConnectionProducer) Close() error {
-	a.Lock()
-	defer a.Unlock()
+    a.Lock()
+    defer a.Unlock()
 
-	a.client = nil
+    a.client = nil
 
-	return nil
+    return nil
 }
